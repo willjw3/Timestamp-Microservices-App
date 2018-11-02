cb8edadbee864236e7dfed68b445cb31418c15da
Glitch (smoggy-porch) <none>
2018-11-02 13:29:50 +0000

    ☝️🏩 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,50 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var moment = require('moment');
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get('/api/timestamp/', function(req, res) {
+  var unixDate = + new Date();
+  var natDate = new Date().toUTCString();
+  res.json({"unix": unixDate, "utc": natDate});
+});
+
+app.get("/api/timestamp/:date_string", (req, res) => {
+  var date = moment(req.params.date_string, ['X', moment.ISO_8601]);
+  if (date.isValid()){
+    var unixDate = date.format("X")*1000;
+    var natDate = date.toDate().toUTCString();
+    res.json({"unix": unixDate, "utc": natDate});
+  }
+  else { res.json({"error": "Invalid Date"})};
+
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
